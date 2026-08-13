6f290ca4d2c28b1015421f6ba8c51162750108cd
Nabil Yusra <nabilazura3@gmail.com>
2026-08-13 13:14:04 +0700

    Add Footer component with copyright notice

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer(){
+    return(
+        <footer className="text-center text-gray-500 mt-10 py-4">
+            &copy; 2026 Vintage Touch. All rights reserved
+        </footer>
+    );
+}
+
+export default Footer;
