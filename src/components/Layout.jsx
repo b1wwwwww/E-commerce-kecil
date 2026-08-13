c75757c7472b4f6cccc18021c328aedff1b6c2b3
Nabil Yusra <nabilazura3@gmail.com>
2026-08-13 12:03:05 +0700

    Update Header title to 'Vintage Touch' and add Layout component

---
+++ src/components/Layout.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer";
+
+function layout(){
+    return(
+        <>
+        <Header />
+        <main><Outlet /></main>
+        <Footer />
+        </>
+    )
+}
+
+export default layout;
