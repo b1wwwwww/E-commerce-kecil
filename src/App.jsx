afcfec26e6683cb073049cf41f9def244a1c3cf7
Nabil Yusra <nabilazura3@gmail.com>
2026-08-25 17:59:30 +0700

    Add routing for Login and Register pages in App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import DetailProduk from "./pages/DetailProduk";
 import Keranjang from "./pages/Keranjang";
+import Login from "./pages/Login";        
+import Register from "./pages/Register";  
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/produk/:id" element={<DetailProduk />} />
           <Route path="/keranjang" element={<Keranjang />} />
+          <Route path="/login" element={<Login />} />        
+          <Route path="/register" element={<Register />} />  
           <Route path="*" element={<h2>404 - Halaman Tidak Ditemukan</h2>} />
         </Route>
       </Routes>
