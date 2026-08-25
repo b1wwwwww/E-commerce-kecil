8e7590b5f66b2dd8f6d5223dc4749d591119c666
Nabil Yusra <nabilazura3@gmail.com>
2026-08-25 21:47:21 +0700

    Wrap Keranjang route with ProtectedRoute to enforce authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 // App.jsx
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
+import ProtectedRoute from "./components/ProtectedRoute"; 
 import Home from "./pages/Home";
 import DetailProduk from "./pages/DetailProduk";
 import Keranjang from "./pages/Keranjang";
-import Login from "./pages/Login";        
-import Register from "./pages/Register";  
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
@@ -14,9 +15,22 @@ function App() {
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
           <Route path="/produk/:id" element={<DetailProduk />} />
-          <Route path="/keranjang" element={<Keranjang />} />
-          <Route path="/login" element={<Login />} />        
-          <Route path="/register" element={<Register />} />  
+
+          {/*
+            Halaman Keranjang sekarang dibungkus ProtectedRoute.
+            Kalau user belum login, otomatis dilempar ke /login sebelum sempat lihat isi Keranjang.
+          */}
+          <Route
+            path="/keranjang"
+            element={
+              <ProtectedRoute>
+                <Keranjang />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
           <Route path="*" element={<h2>404 - Halaman Tidak Ditemukan</h2>} />
         </Route>
       </Routes>
